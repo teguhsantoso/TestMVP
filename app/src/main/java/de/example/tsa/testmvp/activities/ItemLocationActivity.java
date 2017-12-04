4000eedaf29f07b0063f13e900cd7739283a25ce
Teguh Santoso <teguh.santoso@gmx.de>
2017-12-04 21:32:52 +0100

    Change permission fine access location. Rework the UI layout. Clean up code.

--- app/src/main/java/de/example/tsa/testmvp/activities/ItemLocationActivity.java
+++ app/src/main/java/de/example/tsa/testmvp/activities/ItemLocationActivity.java
@@ -1,35 +1,58 @@
 package de.example.tsa.testmvp.activities;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 import de.example.tsa.testmvp.R;
+import de.example.tsa.testmvp.services.Constants;
 
 import static de.example.tsa.testmvp.services.Constants.PERMISSION_REQUEST_ACCESS_LOCATION;
 
 public class ItemLocationActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+    /**
+     *
+     * Permission for finding device location.
+     * Location of any device can be determined by two ways:
+     * 1. Using NETWORK_PROVIDER (provides better and accurate locations)
+     *    Android permissions required for using this provider are either ACCESS_COARSE_LOCATION or ACCESS_FINE_LOCATION.
+     * 2. Using GPS_PROVIDER (less accurate locations)
+     *    Android permissions required for using this provider is only ACCESS_FINE_LOCATION.
+     *
+     */
+
     private FusedLocationProviderClient mFusedLocationClient;
     private Location                    mLastLocation;
+    private LocationRequest             mLocationRequest;
     private View                        rootLayout;
+    private TextView                    textViewCurrentPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_location);
         this.rootLayout = findViewById(R.id.root_layout);
+        this.textViewCurrentPosition = findViewById(R.id.textViewCurrentPosition);
         this.mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
@@ -41,7 +64,10 @@ public class ItemLocationActivity extends AppCompatActivity implements ActivityC
     @Override
     protected void onResume() {
         super.onResume();
-        getLastLocation();
+
+        // Find last device position.
+        getLastLocationAndUpdatePosition();
+
     }
 
     @Override
@@ -70,34 +96,73 @@ public class ItemLocationActivity extends AppCompatActivity implements ActivityC
         super.onDestroy();
     }
 
-    private void getLastLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            // Permission is already available, start camera preview
-            findLastDevicePosition();
-        } else {
-            // Permission is missing and must be requested.
-            requestAccessLocationPermissions();
-        }
+    private void getLastLocationAndUpdatePosition() {
+        findLastDevicePosition();
+        startLocationUpdates();
     }
 
-    @SuppressLint("MissingPermission")
     private void findLastDevicePosition() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is missing and must be requested.
+            requestAccessLocationPermissions();
+            return;
+        }
+
         mFusedLocationClient.getLastLocation().addOnCompleteListener(this, new OnCompleteListener<Location>() {
                     @Override
                     public void onComplete(@NonNull Task<Location> task) {
-                        if (task.isSuccessful() && task.getResult() != null) {
-                            mLastLocation = task.getResult();
-                            Snackbar.make(rootLayout, "Last location: " + mLastLocation.getLongitude() + ", " + mLastLocation.getLatitude(), Snackbar.LENGTH_SHORT).show();
-                        } else {
+                        if (task.isSuccessful() && task.getResult() == null) {
                             Snackbar.make(rootLayout, "No location detected.", Snackbar.LENGTH_SHORT).show();
+                            return;
                         }
+
+                        // Retrieve the last device position.
+                        mLastLocation = task.getResult();
+                        Snackbar.make(rootLayout, "Last location: " + mLastLocation.getLatitude() + ", " + mLastLocation.getLongitude(), Snackbar.LENGTH_SHORT).show();
+
                     }
                 });
     }
 
+    private void startLocationUpdates() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is missing and must be requested.
+            requestAccessLocationPermissions();
+            return;
+        }
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        mLocationRequest.setInterval(Constants.UPDATE_INTERVAL_MILLIS);
+        mLocationRequest.setFastestInterval(Constants.FASTEST_INTERVAL_MILLIS);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+        settingsClient.checkLocationSettings(locationSettingsRequest);
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                onLocationChanged(locationResult.getLastLocation());
+            }
+        }, Looper.myLooper());
+    }
+
+    public void onLocationChanged(Location location) {
+        if (location == null) {
+            return;
+        }
+        Log.d(Constants.LOGGER, ">>> Update listener device position: " + location.getLatitude() + ", " + location.getLongitude());
+        this.textViewCurrentPosition.setText("Current position: " + location.getLatitude() + ", " + location.getLongitude());
+    }
+
     private void requestAccessLocationPermissions() {
         // Permission has not been granted and must be requested.
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
             // Provide an additional rationale to the user if the permission was not granted
             // and the user would benefit from additional context for the use of the permission.
             // Display a SnackBar with a button to request the missing permission.
@@ -105,13 +170,13 @@ public class ItemLocationActivity extends AppCompatActivity implements ActivityC
                 @Override
                 public void onClick(View view) {
                     // Request the permission
-                    ActivityCompat.requestPermissions(ItemLocationActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_ACCESS_LOCATION);
+                    ActivityCompat.requestPermissions(ItemLocationActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_ACCESS_LOCATION);
                 }
             }).show();
         } else {
             Snackbar.make(rootLayout, "Permission is not available. Requesting access coarse permission.", Snackbar.LENGTH_SHORT).show();
             // Request the permission. The result will be received in onRequestPermissionResult().
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_ACCESS_LOCATION);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_ACCESS_LOCATION);
         }
     }
 
--- app/src/main/java/de/example/tsa/testmvp/services/Constants.java
+++ app/src/main/java/de/example/tsa/testmvp/services/Constants.java
@@ -5,10 +5,12 @@ package de.example.tsa.testmvp.services;
  */
 
 public class Constants {
-    public static final String     LOGGER = "LOGGER";
-    public static final int        PERMISSION_REQUEST_ACCESS_LOCATION = 0;
-    public static final String     GERMAN_PATTERN_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
-    public static final String     INTENT_SELECTED_PRODUCT = "selected_product";
-    public static final String     INTENT_ACTION_READ = "de.tsa.mvp.READ";
-    public static final String     INTENT_EXTRA_RESPONSE = "intent-response";
+    public static final String      LOGGER = "LOGGER";
+    public static final int         PERMISSION_REQUEST_ACCESS_LOCATION = 0;
+    public static final long        UPDATE_INTERVAL_MILLIS = 10 * 1000;
+    public static final long        FASTEST_INTERVAL_MILLIS = 2 * 1000;
+    public static final String      GERMAN_PATTERN_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final String      INTENT_SELECTED_PRODUCT = "selected_product";
+    public static final String      INTENT_ACTION_READ = "de.tsa.mvp.READ";
+    public static final String      INTENT_EXTRA_RESPONSE = "intent-response";
 }
