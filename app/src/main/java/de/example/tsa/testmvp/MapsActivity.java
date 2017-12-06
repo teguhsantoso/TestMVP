ccd7b64941967aa0b349d0c3480fdbd0321ee75c
Teguh Santoso <teguh.santoso@gmx.de>
2017-12-06 17:35:45 +0100

    Add listener for last location device position. Rework some UI elements. Add fragment map inside reltive layout.

--- app/src/main/java/de/example/tsa/testmvp/MapsActivity.java
+++ app/src/main/java/de/example/tsa/testmvp/MapsActivity.java
@@ -1,27 +1,91 @@
 package de.example.tsa.testmvp;
 
-import android.support.v4.app.FragmentActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import static de.example.tsa.testmvp.services.Constants.PERMISSION_REQUEST_ACCESS_LOCATION;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
-    private GoogleMap mMap;
+    private View            rootLayout;
+    private TextView        textViewCurrentPosition;
+    private GoogleMap       mMap;
+    private                 FusedLocationProviderClient mFusedLocationClient;
+    private                 Location mLastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        this.rootLayout = findViewById(R.id.root_layout);
+
+        this.textViewCurrentPosition = findViewById(R.id.textViewCurrentPosition);
+
+        this.mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Find last device position.
+        //getLastLocationAndUpdatePosition();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.trans_right_in, R.anim.trans_right_out);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 
 
@@ -37,10 +101,71 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.getUiSettings().setRotateGesturesEnabled(true);
+        googleMap.getUiSettings().setScrollGesturesEnabled(true);
+        googleMap.getUiSettings().setTiltGesturesEnabled(true);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Try finding last position of device.
+        getLastLocationAndUpdatePosition();
+
     }
+
+    private void getLastLocationAndUpdatePosition() {
+        findLastDevicePosition();
+        //startLocationUpdates();
+    }
+
+    private void requestAccessLocationPermissions() {
+        // Permission has not been granted and must be requested.
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // Display a SnackBar with a button to request the missing permission.
+            Snackbar.make(rootLayout, "Coarse access is required to display the item position.", Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Request the permission
+                    ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_ACCESS_LOCATION);
+                }
+            }).show();
+        } else {
+            Snackbar.make(rootLayout, "Permission is not available. Requesting access coarse permission.", Snackbar.LENGTH_SHORT).show();
+            // Request the permission. The result will be received in onRequestPermissionResult().
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_ACCESS_LOCATION);
+        }
+    }
+
+    private void findLastDevicePosition() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is missing and must be requested.
+            requestAccessLocationPermissions();
+            return;
+        }
+
+        mFusedLocationClient.getLastLocation().addOnCompleteListener(this, new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if (task.isSuccessful() && task.getResult() == null) {
+                    Snackbar.make(rootLayout, "No location detected.", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Retrieve the last device position.
+                mLastLocation = task.getResult();
+                textViewCurrentPosition.setText("Last item position: " + mLastLocation.getLatitude() + ", " + mLastLocation.getLongitude());
+
+                // Refresh fragment map and put marker.
+                LatLng itemPosition = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+
+                CameraUpdate cameraPosition = CameraUpdateFactory.newLatLngZoom(itemPosition, 16);
+                mMap.addMarker(new MarkerOptions().position(itemPosition).title("Item Position"));
+                mMap.moveCamera(cameraPosition);
+                mMap.animateCamera(cameraPosition);
+
+            }
+        });
+    }
+
 }
